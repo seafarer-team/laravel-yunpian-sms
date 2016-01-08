88e14e46a55158417fde2c58cb86783be889cc25
dusthub <dusthub@seafarer.me>
2016-01-08 21:31:14 +0800

    init perfect

---
+++ src/Seafarer/YunpianSms/YunpianSmsServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Seafarer\YunpianSms;
+
+use Illuminate\Support\ServiceProvider;
+
+class YunpianSmsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('seafarer/yunpian-sms');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
